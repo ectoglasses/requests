039c31f47bb29dea3e5ea91fd695b5a49f7d0a64
ectoglasses <ropun.tsd@gmail.com>
2021-07-01 16:03:23 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ packages = ['requests']
 
 requires = [
     'chardet @ git+https://github.com/ectoglasses/chardet.git#egg=chardnet',
-    'idna @ git+https://github.com/ectoglasses/idna.git#egg=idna`',
-    'urllib3 @ git+https://github.com/ectoglasses/urllib3.git#egg=flask',
+    'idna @ git+https://github.com/ectoglasses/idna.git#egg=idna',
+    'urllib3 @ git+https://github.com/ectoglasses/urllib3.git#egg=urllib3',
     'certifi @ git+https://github.com/ectoglasses/python-certifi.git#egg=certifi'
 
 ]
